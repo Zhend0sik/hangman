faa1a0b95ad5abe1163c26604bf8bb30d1c326ab
Evgeny <49392194+Zhend0sik@users.noreply.github.com>
2020-06-18 14:20:23 +0500

    master

--- python_ex/python_ex005.py
+++ python_ex/python_ex005.py
@@ -4,3 +4,13 @@ import math
 l = 4
 w = 10
 d = math.sqrt(l**2 + w**2)
+
+for i in range(100):
+    print("Привет, мир!")
+
+# Это программа javaScript.
+# Она не будет работать.    
+for (i = 0; i < 100; i++) {
+    console.log("Привет, мир!");
+}
+
